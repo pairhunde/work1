ce8a724a984a12b7021cb71b5205427751915fdb
pairhunde <59679978+pairhunde@users.noreply.github.com>
2020-01-09 06:19:42 +0000

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -10,7 +10,7 @@ $email = $_SESSION['id'];
 $amount = 5000000;  //the amount in kobo. This value is actually NGN 50,000
 
 // url to go to after payment
-$callback_url = 'https://airhco.org/callback.php';  
+$callback_url = 'https://website/callbackpage.php';  
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.paystack.co/transaction/initialize",
@@ -22,7 +22,7 @@ curl_setopt_array($curl, array(
     'callback_url' => $callback_url
   ]),
   CURLOPT_HTTPHEADER => [
-    "authorization: Bearer sk_live_f95a33c56cd4fdad8b11b173853c369e57683786", //replace this with your own test key
+    "authorization: Bearer sk_live_f95a336473462323434fdad8b11b173853c369e57683786", //replace this with your own test key
     "content-type: application/json",
     "cache-control: no-cache"
   ],
