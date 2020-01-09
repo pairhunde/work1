24876849ce2caaf0181cc1a1d1a15689b8b45a94
pairhunde <59679978+pairhunde@users.noreply.github.com>
2020-01-09 06:17:51 +0000

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost","airhyoct_airhyoct","Gunmen@123","airhyoct_airhco");
+$conn = mysqli_connect("localhost","username","password","dbname");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
